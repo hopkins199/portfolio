4e60bdbf29cf6f60b31b1e35c594d2b0886cbb55
drew hopkins <hopkins199@gmail.com>
2022-08-31 11:18:15 -0700

    updates to site and creation of silly quiz

---
+++ buzz-feed/app.js
@@ -0,0 +1,235 @@
+const questionDisplay = document.querySelector('#questions')
+const answerDisplay = document.querySelector('#answer')
+
+const questions = [
+    {
+        id: 0,
+        text: "Pick a vacation destination",
+        answers: [
+            {
+                text: "New York",
+                image: "https://images.unsplash.com/photo-1555109307-f7d9da25c244?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Kit Suman",
+            },
+            {
+                text: "Austin",
+                image: "https://images.unsplash.com/photo-1531218150217-54595bc2b934?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80",
+                alt: "Time lapse photo of cars on bridge",
+                credit: "Carlos Alfonzo",
+            },
+            {
+                text: "Portland",
+                image: "https://images.unsplash.com/photo-1541457523724-95f54f7740cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                alt: "high rise buildings",
+                credit: "Elena Kuchko",
+            },
+            {
+                text: "New Orleans",
+                image: "https://images.unsplash.com/photo-1549965738-e1aaf1168943?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+                alt: "road with people and house",
+                credit: "João Francisco",
+            },
+        ]
+    },
+    {
+        id: 1,
+        text: "Pick some food",
+        answers: [
+            {
+                text: "Pizza",
+                image: "https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
+                alt: "Pepperoni pizza",
+                credit: "Alan Hardman",
+            },
+            {
+                text: "Sandwich",
+                image: "https://images.unsplash.com/photo-1619096252214-ef06c45683e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80",
+                alt: "burger with tomato with lettuce on ceramic plate",
+                credit: "Seriously Low Carb",
+            },
+            {
+                text: "Pasta",
+                image: "https://images.unsplash.com/photo-1551892374-ecf8754cf8b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+                alt: "pasta in white ceramic bowl",
+                credit: "Krista Stucchio",
+            },
+            {
+                text: "Hamburger",
+                image: "https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80",
+                alt: "double patty cheeseburger",
+                credit: "amirali mirhashemian",
+            },
+        ]
+    },
+    {
+        id: 2,
+        text: "pick a home",
+        answers: [
+            {
+                text: "Traditional",
+                image: "https://images.unsplash.com/photo-1574664598429-9d95d9486688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                alt: "tall yellow house",
+                credit: "Burgess Milner",
+            },
+            {
+                text: "Modern",
+                image: "https://images.unsplash.com/photo-1600585153490-76fb20a32601?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                alt: "Concrete home",
+                credit: "R ARCHITECTURE",
+            },
+            {
+                text: "House",
+                image: "https://images.unsplash.com/photo-1582063289852-62e3ba2747f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                alt: "brown and white house",
+                credit: "Ann Wallace",
+            },
+            {
+                text: "Mountain",
+                image: "https://images.unsplash.com/photo-1475087542963-13ab5e611954?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                alt: "cabin in woods",
+                credit: "Mike Petrucci",
+            },
+        ]
+    }
+]
+
+const answers = [
+    {
+        combination: ["New York", "Pizza", "Traditional"],
+        text: "Blue Cheese",
+        image: "https://images.unsplash.com/photo-1626957341926-98752fc2ba90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        alt: "Blue Cheese"
+    },
+    {
+        combination: ["Austin", "Pasta", "Modern"],
+        text: "Cheddar Cheese",
+        image: "https://images.unsplash.com/photo-1574175679306-4c5f33146b6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+        alt: "Cheddar Cheese"
+    },
+    {
+        combination: ["Portland", "Sandwich", "House"],
+        text: "Feta Cheese",
+        image: "https://images.unsplash.com/photo-1559561853-08451507cbe7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1257&q=80",
+        alt: "Feta Cheese"
+    },
+    {
+        combination: ["New Orleans", "Hamburger", "Mountain"],
+        text: "Halloumi",
+        image: "https://images.unsplash.com/photo-1453078977505-10c3e375c2a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+        alt: "Halloumi"
+    },
+]
+
+const unansweredQuestions = []
+const chosenAnswers = []
+
+const populateQuestions = () => {
+    questions.forEach(question => {
+        const titleBlock = document.createElement('div')
+        titleBlock.id = question.id
+        titleBlock.classList.add('title-block')
+        const titleHeading = document.createElement('h2')
+        titleHeading.textContent = question.text
+        titleBlock.append(titleHeading)
+        questionDisplay.append(titleBlock)
+
+        const answersBlock = document.createElement('div')
+        answersBlock.id = question.id + "-questions"
+        answersBlock.classList.add('answer-options')
+
+        unansweredQuestions.push(question.id)
+
+        question.answers.forEach(answer => {
+            const answerBlock = document.createElement('div')
+            answerBlock.classList.add('answer-block')
+            answerBlock.addEventListener('click', () => handleClick(question.id, answer.text))
+            const answerImage = document.createElement('img')
+            answerImage.setAttribute('src', answer.image)
+            answerImage.setAttribute('alt', answer.alt)
+
+            const answerTitle = document.createElement('h3')
+            answerTitle.textContent = answer.text
+
+            const answerInfo = document.createElement('p')
+            const imageLink = document.createElement('a')
+            imageLink.setAttribute('href', answer.image)
+            imageLink.textContent = answer.credit
+            const sourceLink = document.createElement('a')
+            sourceLink.textContent = 'Unsplash'
+            sourceLink.setAttribute('src', 'https://www.unsplash.com')
+            answerInfo.append(imageLink, ' to ', sourceLink)
+
+
+            answerBlock.append(answerImage, answerTitle, answerInfo)
+
+            answersBlock.append(answerBlock)
+        })
+
+        questionDisplay.append(answersBlock)
+    })
+}
+populateQuestions()
+
+const handleClick = (questionId, chosenAnswer) => {
+    if(unansweredQuestions.includes(questionId))
+        chosenAnswers.push(chosenAnswer)
+        const itemToRemove = unansweredQuestions.indexOf(questionId)
+
+    if (itemToRemove > -1) {
+        unansweredQuestions.splice(itemToRemove, 1)
+    }
+
+    console.log(unansweredQuestions)
+
+
+    disableQuestionBlock(questionId, chosenAnswer)
+    const lowestQuestionId = Math.min(...unansweredQuestions)
+    location.href = '#' + lowestQuestionId
+
+    if (!unansweredQuestions.length) {
+        location.href = '#answer'
+        showAnswer()
+    }
+}
+
+const showAnswer = () => {
+    let result
+
+    answers.forEach(answer => {
+        if (chosenAnswers.includes(answer.combination[0]) +
+            chosenAnswers.includes(answer.combination[1]) +
+            chosenAnswers.includes(answer.combination[2])
+        ) {
+            result = answer
+        } else if (!result){
+            result = answers[0]
+        }
+    })
+
+    const answerBlock = document.createElement('div')
+    answerBlock.classList.add('result-block')
+    const answerTitle = document.createElement('h3')
+    answerTitle.textContent = result.text
+    const answerImage = document.createElement('img')
+    answerImage.setAttribute('src',result.image)
+    answerImage.setAttribute('alt', result.alt)
+
+    answerBlock.append(answerTitle, answerImage)
+
+    answerDisplay.append(answerBlock)
+
+    const allAnswerBlocks = document.querySelectorAll('.answer-block')
+    Array.from(allAnswerBlocks).forEach(answerBlock => answerBlock.replaceWith(answerBlock.cloneNode(true)))
+}
+
+const disableQuestionBlock = (questionId, chosenAnswer) => {
+    const currentQuestionBlock = document.getElementById(questionId + "-questions")
+
+    Array.from(currentQuestionBlock.children).forEach(block => {
+        if (block.children.item(1).innerText !== chosenAnswer) {
+            block.style.opacity = "50%"
+        }
+    })
+}
+
